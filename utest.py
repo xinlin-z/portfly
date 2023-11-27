b245f268a18b8b41a93d3a784841c227e5d46f02
xinlin <xinlin.zhang@hotmail.com>
2023-11-27 21:18:16 +0800

    update

--- utest.py
+++ utest.py
@@ -5,5 +5,8 @@ random.seed()
 
 for i in range(1000):
     bmsg = bytes(random.randint(0,255) for _ in range(i))
+    # print('##', i, bmsg)
+    # print(1, cx(bmsg))
+    # print(2, dx(cx(bmsg)))
     assert dx(cx(bmsg)) == bmsg
 
